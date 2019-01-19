61a9fbd696a3562da5db53c10cd512f6695368bd
Szymon Leus <szymonleus@op.pl>
2019-01-19 15:17:04 +0100

    Added "for" loop.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        for (int x = 1; x < 11; x++) {
+            System.out.println(x);
+        }
     }
 }
